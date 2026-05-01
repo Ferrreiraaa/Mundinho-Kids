ecfe19e2544b65d610c3e59844921ccc18354fc8
Eduardo Ferreira <149728880+Ferrreiraaa@users.noreply.github.com>
2026-05-01 15:16:47 -0300

    Update catalogo link and add autismo button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const links = {
   whatsapp: "https://wa.me/message/QF4YXSXDZ3PDJ1",
-  catalogo: "#",
+  catalogo: "https://www.mundinhokids.com",
   visita: "https://docs.google.com/forms/d/e/1FAIpQLSfux2jwFEpjWIqPMi_yYLxOVFYODmSTmLYVbRU9dZbmLpj0Mw/viewform",
+  autismo: "https://docs.google.com/forms/d/e/1FAIpQLSex---PeuKLx47_l1l-MWb-rE4I_7HzrGZc9yrpbzsozaoBpg/viewform",
   instagram: "https://www.instagram.com/mundinhokidsrp/"
 };
 
@@ -9,11 +10,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const whatsappBtn = document.querySelector(".whatsapp");
   const catalogoBtn = document.querySelector(".catalogo");
   const visitaBtn = document.querySelector(".visita");
+  const autismoBtn = document.querySelector("autismo");
   const instagramBtn = document.querySelector(".instagram");
 
   if (whatsappBtn) whatsappBtn.href = links.whatsapp;
   if (catalogoBtn) catalogoBtn.href = links.catalogo;
   if (visitaBtn) visitaBtn.href = links.visita;
+  if (autismoBtn) autismoBtn.href = links.autismo;
   if (instagramBtn) instagramBtn.href = links.instagram;
 
   const botoes = document.querySelectorAll(".link-btn");
